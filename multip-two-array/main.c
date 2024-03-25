0e48ffab48524bb1885586bd064c0cd6179eecf7
Kyotun <emirpisirici@hotmail.com>
2024-03-25 10:33:31 +0100

    Created two int array.

--- multip-two-array/main.c
+++ multip-two-array/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hey");
+    // rows-> 0,1,2,3 | columns -> 0,1,2
+    int r1,c1 = 0;
+    int arr_1[4][3] = {{1,2,3},{4,5,6},{7,8,9},{1,2,3}};
+
+    // rows-> 0,1,2 | columns -> 0,1,2,3,4
+    int r2,c2 = 0;
+    int arr_2[3][5] = {{1,2,3,4,3},{4,3,2,1,1},{2,2,2,2,2}};
+    
     return 0;
 }
