5a2b869e3145f248a308abac85f5887b51586b68
Kyotun <emirpisirici@hotmail.com>
2024-03-25 12:38:57 +0100

    Ask user to give entry for creation of a 2D array.

--- array-basic-stdio/main.c
+++ array-basic-stdio/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
+    int r1, c1, r2, c2;
+
+    printf("Please enter the number of rows of the first array:");
+    scanf("%d", &r1);
+    printf("Please enter the number of columns of the first array:");
+    scanf("%d", &c1);
+
+    printf("Please enter the number of rows of the second array:");
+    scanf("%d", &r2);
+    printf("Please enter the number of columns of the second array:");
+    scanf("%d", &c2);
+
+    int arr_one[r1][c1];
+    
     return 0;
 }
