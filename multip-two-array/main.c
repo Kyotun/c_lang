3c55225dbf116579edd8464b2eb80d8a68e2cae3
Kyotun <emirpisirici@hotmail.com>
2024-03-25 10:36:09 +0100

    Added instructions for to take dimensions of arrays.

--- multip-two-array/main.c
+++ multip-two-array/main.c
@@ -8,6 +8,16 @@ int main(void) {
     // rows-> 0,1,2 | columns -> 0,1,2,3,4
     int r2,c2 = 0;
     int arr_2[3][5] = {{1,2,3,4,3},{4,3,2,1,1},{2,2,2,2,2}};
-    
+
+    printf("Please enter the number of rows of the first array:");
+    scanf("%d", &r1);
+    printf("Please enter the number of columns of the first array:");
+    scanf("%d", &c1);
+
+    printf("Please enter the number of rows of the second array:");
+    scanf("%d", &r2);
+    printf("Please enter the number of columns of the second array:");
+    scanf("%d", &c2);
+
     return 0;
 }
